a712c019a11cd838fd211ed25ae06a1eeb6dc38c
IlievIvo <134727053+IlievIvo@users.noreply.github.com>
2025-02-02 11:57:08 +0100

    Add files via upload
    Added empty project

---
+++ kollegaX/src/main/java/com/example/kollegaX/KollegaXApplication.java
@@ -0,0 +1,15 @@
+package com.example.kollegaX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KollegaXApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test");
+		SpringApplication.run(KollegaXApplication.class, args);
+	}
+
+}
---
+++ kollegaX/src/test/java/com/example/kollegaX/KollegaXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kollegaX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KollegaXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
